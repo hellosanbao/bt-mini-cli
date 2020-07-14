105028445828292a38f9b4925467e335fe9e8532
熊志强 <zhiqiang.xiong@beantechs.com>
2020-07-14 12:10:59 +0800

    修改wxml模板

--- temp.js
+++ temp.js
@@ -2,7 +2,7 @@ let app = getApp()
 Page({
     data:{},
     onLoad(){},
-    onSHow(){},
+    onShow(){},
     onPullDownRefresh(){},  //下拉刷新 （请确保json配置中的enablePullDownRefresh开启）
     onReachBottom(){},   //触底事件 （请确保json配置中的onReachBottomDistance开启）
     onShareAppMessage(){  //分享事件，如果页面不需要分享请删除该方法
